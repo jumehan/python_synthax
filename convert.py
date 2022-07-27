4cc611e8fa0be0e62637753697b9c95e677aba7f
Julie Han <juliemenghan@gmail.com>
2022-07-26 20:17:03 -0400

    updated

--- convert.py
+++ convert.py
@@ -17,21 +17,22 @@ def convert_temp(unit_in, unit_out, temp):
     if unit_in == "c" and unit_out == "f":
         new_temp_f = temp * 1.8 + 32
         return new_temp_f
-    if unit_in == "f" and unit_out == "c":
+    elif unit_in == "f" and unit_out == "c":
         new_temp_c = (temp - 32) * 5/9
         return new_temp_c
-    if unit_in == unit_out:
+    elif unit_in == unit_out:
         new_temp = temp
         return new_temp
-    if unit_in != "c" or "f":
+    elif unit_in != "c" or unit_in != "f":
         return f"Invalid unit {unit_in}"
-    if unit_out != "c" or "f":
+    else:
         return f"Invalid unit {unit_out}"
 
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
-print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
+print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
+                                
